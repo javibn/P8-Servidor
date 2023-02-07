6f4c9649079b5a50bd433581674726cd73167194
JaviGva <javbernav@alu.edu.gva.es>
2023-02-07 18:23:12 +0100

    practica terminada

---
+++ ApiContactos/ApiContactos/Migrations/20230207155837_Migracion_Clase.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ApiContactos.Migrations
+{
+    public partial class Migracion_Clase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
